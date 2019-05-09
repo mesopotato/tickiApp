a2d494e9735f52e62e4d0e5d2cc33312c56263eb
mesopotato <dittrich.yannick@gmail.com>
2019-05-09 18:58:05 +0200

    many pfusch

--- App.js
+++ App.js
@@ -1,39 +1,103 @@
 import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, TouchableOpacity } from 'react-native';
+
+
+import { FontAwesome } from '@expo/vector-icons';
+
 import {fetchTickets } from './constants/api';
+import {loginNow } from './constants/api';
 
 export default class App extends React.Component {
   static defaultProps = {
-    fetchTickets
+    //fetchTickets
+    loginNow
   }
 
   state = {
     loading: false,
-    events: []
+    events: [],
+    didError: false
+  }
+  onLogin() {
+    const { name, password } = this.state;
+    this.setState({loading: true});
+    Alert.alert('Credentials', `${name} + ${password}`);
+    const answer = await this.props.loginNow(name, password);
+    if (answer.user == 'NO'){
+      this.setState({wrongUserName: true})
+    }
+    if (answer.user == 'MISMATCH'){
+      this.setState({wrongPwd: true})
+    }
+    if(answer.token){
+      this.setState({token: answer.token})
+    }
+    this.setState({ loading: false})
   }
 
   async componentDidMount() {
     this.setState({loading: true});
-    const data = await this.props.fetchTickets();
+    //const data = await this.props.fetchTickets();
     this.setState({ loading: false, events: data.events });
   }
+
   render() {
     if (this.state.loading){
-      return (
-        <View style={styles.container}>
-          <ActivityIndicator size='large'/>
-        </View>
-      )
+      loadingView();
     }
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        {this.state.events.map((event, i) => (
-          <Text key={i}>{event.title}</Text>
-        ))}
+        <TextInput
+          value={this.state.name}
+          onChangeText={(name) => this.setState({ name })}
+          placeholder={'Username'}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        
+        <Button
+          title={'Login'}
+          style={styles.input}
+          onPress={this.onLogin.bind(this)}
+        />
       </View>
     );
   }
+};
+
+displayMainview = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      {this.state.events.map((event, i) => (
+        <Text key={i}>{event.title}</Text>
+      ))}
+    </View>
+  );
+}
+
+displayError = () => {
+  return (
+    <View style={styles.userInfo}>
+      <Text style={styles.errorText}>
+        There was an error, please try again.
+      </Text>
+    </View>
+  );
+}
+
+loadingView = () => {
+  return (
+      <View style={styles.container}>
+        <ActivityIndicator size='large'/>
+      </View>  
+  );
 }
 
 const styles = StyleSheet.create({
@@ -43,4 +107,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#2FD566',
+    padding: 20
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 20
+  },
+  userInfo: {
+    height: 250,
+    width: 200,
+    alignItems: 'center',
+  },
+  userInfoText: {
+    color: '#fff',
+    fontSize: 18
+  },
+  errorText: {
+    color: '#fff',
+    fontSize: 18
+  },
+  profileImage: {
+    height: 64,
+    width: 64,
+    marginBottom: 32
+  }
 });
--- constants/api.js
+++ constants/api.js
@@ -1,3 +1,30 @@
+import { AuthSession } from 'expo';
+
 export const fetchTickets = () =>
     fetch('http://10.0.2.2:3000/api/getEvents')
         .then(res => res.json());
+
+export const loginNow = (name, password) =>
+    fetch(`http://10.0.2.2:3000/api/login/${name}&${password}`)
+        .then(res => res.json());
+
+export var handleLogin = async (name, password) => {
+    let redirectUrl = AuthSession.getRedirectUrl();
+    let results = await AuthSession.startAsync({
+        authUrl:
+            `http://10.0.2.2:3000/api/login/${name}
+            &${password}
+              &${encodeURIComponent(redirectUrl)}`
+    });
+    // Defined in next step
+    if (results.type !== 'success') {
+        this.setState({ didError: true });
+    } else {
+        const userInfo = await axios.get(`https://api.spotify.com/v1/me`, {
+            headers: {
+                "Authorization": `Bearer ${results.params.access_token}`
+            }
+        });
+        this.setState({ userInfo: userInfo.data });
+    }
+};
